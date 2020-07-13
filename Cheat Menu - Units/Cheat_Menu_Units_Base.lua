57af8a25f9e97b866a875f2e3c2306d42c8a1a03
Matthew Lonis <mlonis@ford.com>
2020-07-13 17:40:48 -0400

    Update Cheat_Menu_Units_Base.lua

--- Cheat Menu - Units/Cheat_Menu_Units_Base.lua
+++ Cheat Menu - Units/Cheat_Menu_Units_Base.lua
@@ -16,20 +16,21 @@ local prevCheatMovePoints = nil;
 
 --******************************************************************************
 function tprint (tbl, indent)
-  if not indent then indent = 0 end
-  for k, v in pairs(tbl) do
-    formatting = string.rep("  ", indent) .. k .. ": "
-    if type(v) == "table" then
-      print(formatting)
-      tprint(v, indent+1)
-    elseif type(v) == 'boolean' then
-      print(formatting .. tostring(v))
-    elseif type(v) == 'userdata' then
-        print(formatting .. tostring(v))
-    else
-      print(formatting .. v)
+    if not indent then indent = 0 end
+        for k, v in pairs(tbl) do
+            formatting = string.rep("  ", indent) .. k .. ": "
+
+        if type(v) == "table" then
+          print(formatting)
+          tprint(v, indent+1)
+        elseif type(v) == 'boolean' then
+          print(formatting .. tostring(v))
+        elseif type(v) == 'userdata' then
+            print(formatting .. tostring(v))
+        else
+          print(formatting .. v)
+        end
     end
-  end
 end
 
 --local ChangeMovesRemaining = UnitManager.ChangeMovesRemaining;
@@ -67,6 +68,7 @@ local function ChangeAutorestoreAttack()
     else 
         autorestoreAttack = true;
     end
+
     return autorestoreAttack;
 end
 
@@ -82,6 +84,7 @@ local function ChangeAutorestoreHealth()
     else 
         autorestoreHealth = true;
     end
+
     return autorestoreHealth;
 end
 
@@ -97,6 +100,7 @@ local function ChangeAutorestoreMove()
     else 
         autorestoreMove = true;
     end
+
     return autorestoreMove;
 end
 
@@ -112,35 +116,12 @@ local function ChangeAutorestorePromo()
     else 
         autorestorePromo = true;
     end
+
     return autorestorePromo;
 end
 
 --******************************************************************************
 local function RefreshGUI(selectedPlayerId, unitId)
-    ----print(" OnCheatMovement BASE COUNT -- ", Events.UnitDamageChanged.Count());
-    --print(" RefreshGUI BASE 1 -- ", selectedPlayerId, unitId);
-    --local unit = nil;
-    --if UnitManager then 
-        --unit = UnitManager.GetUnit( selectedPlayerId, unitId ); 
-        --print(" RefreshGUI BASE 2 -- ", unit);
-    --end
-    --if unit ~= nil and Players[selectedPlayerId]:IsHuman() then
-        --local uDamage = unit:GetDamage();
-        --local uMDamage = unit:GetMaxDamage();
-        --if uDamage ~= 0 then
-            --unit:SetDamage(0);
-            --unit:SetDamage(uDamage);
-        --elseif uMDamage > 1 then
-            --unit:SetDamage(1);
-            --unit:SetDamage(uDamage);
-        --end
-        
-        --print(" RefreshGUI BASE 3 -- ", uDamage);
-        --Events.UnitDamageChanged(selectedPlayerId, unitId, uDamage, uDamage);
-        --Events.UnitPromoted(selectedPlayerId, unitId);
-        --LuaEvents.TutorialUIRoot_DisableActions(  "UNITOPERATION_RETRAIN", "UNIT_SETTLER" );	
-        --print(" RefreshGUI BASE 4 -- ", uDamage, uMDamage);
-    --end
     if ExposedMembers.MOD_Cheat_Menu_Units.UiRefreshUnitPanel ~= nil then
         ExposedMembers.MOD_Cheat_Menu_Units.UiRefreshUnitPanel();
     end
@@ -157,51 +138,48 @@ end
 local function IsDestinationReached(PlayerID, UnitID)
     if PlayerID ~= nil and UnitID ~= nil and Players[PlayerID] ~= nil and Players[PlayerID]:IsHuman() then
         local unit = nil;
+
         if UnitManager then 
             unit = UnitManager.GetUnit( PlayerID, UnitID ); 
         end
 
         if unit ~= nil then 
             local unitMovesRem, queuedEndPlotId = ExposedMembers.MOD_Cheat_Menu_Units.GetUnitMovesFromUI(PlayerID, UnitID);
+
             if queuedEndPlotId then
-                --print( "IsDestinationReached 0: ", queuedEndPlotId );
-                --if Map.IsPlot(queuedEndPlotId) then
-                    --print( "Cheat OnMovementComplete Destination 1: ", queuedEndPlotId );
                     local plot :table	= Map.GetPlotByIndex(queuedEndPlotId);
-                    --print( "Cheat OnMovementComplete Destination 2: ", plot );
+
                     if  plot ~= nil and (plot:GetX()~=unit:GetX() or plot:GetY()~=unit:GetY())  then 
-                        --print( "Destination was not reached: ", plot );
-                        -- destination was not reached
                         return false;
-                    else
-                            
                     end
-                --end
             end
         end
     end
+
     return true;
 end
 
 --******************************************************************************
 local function OnCombat(m_combatResults)
     if autorestoreAttack or autorestoreHealth then 
-        --print( "On Combat Event :" );
         local cId = m_combatResults[CombatResultParameters.ATTACKER][CombatResultParameters.ID];
+
         if cId ~= nil then
             local unitId = cId.id;
             local playerId = cId.player;
-
             local unit = nil;
+
             if UnitManager then 
                 unit = UnitManager.GetUnit( playerId, unitId ); 
             end
+
             if unit ~= nil and Players[playerId]:IsHuman() then
                 if autorestoreAttack then 
                     print( "Cheat Autorestore Unit Attack Executed" );
                     UnitManager.RestoreUnitAttacks(unit);
                     local unitMovesRem = ExposedMembers.MOD_Cheat_Menu_Units.GetUnitMovesFromUI(playerId, unitId);
                     UnitManager.ChangeMovesRemaining(unit, 1);
+
                     if unitMovesRem < 8 then
                         UnitManager.ChangeMovesRemaining(unit, 4);
                     end
@@ -215,20 +193,23 @@ local function OnCombat(m_combatResults)
                     UnitManager.RestoreMovement(unit);
                     UnitManager.ChangeMovesRemaining(unit, targetMovePoints);
                 end
+
                 RefreshGUI();
+
             end
         end
     end
-    --tprint(m_combatResults, 8);
 end
 
 --******************************************************************************
 function OnUnitDamageChanged(PlayerID, UnitID, newDamage, prevDamage)
     if autorestoreHealth and PlayerID ~= nil and PlayerID ~= -1 and newDamage ~= 0 then 
         local unit = nil;
+
         if UnitManager then 
             unit = UnitManager.GetUnit( PlayerID, UnitID ); 
         end
+
         if unit ~= nil and Players[PlayerID] ~= nil and Players[PlayerID]:IsHuman() then
             print( "Cheat Autorestore Unit Health Executed" );
             unit:SetDamage(0);
@@ -246,44 +227,21 @@ end
 local function OnMovementComplete(PlayerID, UnitID, x, y)
     if autorestoreMove and PlayerID ~= nil and UnitID ~= nil and Players[PlayerID] ~= nil and Players[PlayerID]:IsHuman() then
         local unit = nil;
+
         if UnitManager then 
             unit = UnitManager.GetUnit( PlayerID, UnitID ); 
         end
 
         if unit ~= nil then 
-                UnitManager.RestoreMovement(unit);
-                --ExposedMembers.MOD_Cheat_Menu_Units.RequestDelayedMovementCheat();
-                UnitManager.ChangeMovesRemaining(unit, targetMovePoints);
+            UnitManager.RestoreMovement(unit);
+            --ExposedMembers.MOD_Cheat_Menu_Units.RequestDelayedMovementCheat();
+            UnitManager.ChangeMovesRemaining(unit, targetMovePoints);
 
-                if not IsDestinationReached(PlayerID, UnitID) then 
-                    -- destination was not reached
-                    LookAtPlot(PlayerID, UnitID);
-                    ExposedMembers.MOD_Cheat_Menu_Units.UiRefreshSelection( PlayerID, UnitID );
-                end
-                --if queuedEndPlotId then
-                    --print( "Cheat OnMovementComplete Destination 0: ", queuedEndPlotId );
-                    ----if Map.IsPlot(queuedEndPlotId) then
-                        ----print( "Cheat OnMovementComplete Destination 1: ", queuedEndPlotId );
-                        --local plot :table	= Map.GetPlotByIndex(queuedEndPlotId);
-                        ----print( "Cheat OnMovementComplete Destination 2: ", plot );
-                        --if  plot ~= nil and (plot:GetX()~=x or plot:GetY()~=y)  then 
-                            --print( "Destination was not reached: ", plot );
-                            ---- destination was not reached
-                            --LookAtPlot(PlayerID, UnitID);
-                            --ExposedMembers.MOD_Cheat_Menu_Units.UiRefreshSelection( PlayerID, UnitID );
-                            ----ExposedMembers.MOD_Cheat_Menu_Units.UiRefreshUnitPanel();
-                        --else
-                            --
-                        --end
-                    ----end
-                --end
-
-                -- Look at unit position
-                --plot = Map.GetPlot( unit:GetX(), unit:GetY() );
-                --UI.LookAtPlot( plot );
-                --LookAtPlot(PlayerID, UnitID);
-                --UnitManager.FinishMoves(unit);
-            --end
+            if not IsDestinationReached(PlayerID, UnitID) then 
+                -- destination was not reached
+                LookAtPlot(PlayerID, UnitID);
+                ExposedMembers.MOD_Cheat_Menu_Units.UiRefreshSelection( PlayerID, UnitID );
+            end
         end
     end
 end
@@ -291,8 +249,8 @@ end
 --******************************************************************************
 local function OnMovementChange(PlayerID, UnitID, MovementPoints)
     if autorestoreMove and  PlayerID ~= nil and UnitID ~= nil and Players[PlayerID] ~= nil and Players[PlayerID]:IsHuman() then
-        
         local unit = nil;
+
         if UnitManager then 
             unit = UnitManager.GetUnit( PlayerID, UnitID ); 
         end
@@ -322,15 +280,17 @@ local function OnMovementChange(PlayerID, UnitID, MovementPoints)
             --UnitManager.FinishMoves(unit);
             RefreshGUI();
         end
+
         ExposedMembers.MOD_Cheat_Menu_Units.UiRefreshUnitPanel();
+
     end
 end
 
 --******************************************************************************
 local function ApplyAutorestoreMovementCheat(PlayerID, UnitID, MovementPoints)
     if autorestoreMove and  PlayerID ~= nil and UnitID ~= nil and Players[PlayerID] ~= nil and Players[PlayerID]:IsHuman() then
-        
         local unit = nil;
+
         if UnitManager then 
             unit = UnitManager.GetUnit( PlayerID, UnitID ); 
         end
@@ -367,8 +327,8 @@ end
 --******************************************************************************
 local function OnUnitEmbarkedStateChanged( PlayerID: number, UnitID : number, EmbarkedState : boolean )
     if autorestoreMove and PlayerID ~= nil and UnitID ~= nil and Players[PlayerID] ~= nil and Players[PlayerID]:IsHuman() then
-    
         local unit = nil;
+
         if UnitManager then 
             unit = UnitManager.GetUnit( PlayerID, UnitID ); 
         end
@@ -377,6 +337,7 @@ local function OnUnitEmbarkedStateChanged( PlayerID: number, UnitID : number, Em
             --print( "Cheat OnUnitEmbarkedStateChanged 11: ", MovementPoints );
             UnitManager.RestoreMovement(unit);
             UnitManager.ChangeMovesRemaining(unit, targetMovePoints);
+
             if not IsDestinationReached(PlayerID, UnitID) then 
                 -- destination was not reached
                 LookAtPlot(PlayerID, UnitID);
@@ -390,11 +351,12 @@ end
 local function OnCheatHeal(selectedPlayerId, unitId)
     -- local units = Players[selectedPlayerId]:GetUnits();
     -- local unit = units:FindID(unitId);
-    
     local unit = nil;
+
     if UnitManager then 
         unit = UnitManager.GetUnit( selectedPlayerId, unitId ); 
     end
+
     if unit ~= nil and Players[selectedPlayerId]:IsHuman() and autorestoreHealth then
         if unit:GetDamage() then 
             print( "Cheat Heal Exposed Executed" );
@@ -409,16 +371,18 @@ end
 local function OnDuplicate(selectedPlayerId, unitId, unitType, overridePlayer, overrideType, overrideDestPlotId, overrideDestPlotX, overrideDestPlotY, ttype, useValidLoc, count, pRelig )
     -- local units = Players[selectedPlayerId]:GetUnits();
     -- local unit = units:FindID(unitId);
-    
     local unit = nil;
     local newUnit = nil;
+
     if UnitManager then 
         unit = UnitManager.GetUnit( selectedPlayerId, unitId ); 
     end
+
     if unit ~= nil and Players[selectedPlayerId]:IsHuman() then
         --print( "Cheat Duplicate Exposed Executed" );
         local x = unit:GetX();
         local y = unit:GetY();
+
         if overrideDestPlotId ~= nil and overrideDestPlotX ~= nil and overrideDestPlotY ~= nil and overrideDestPlotX ~= 0 and overrideDestPlotY ~= 0 then
             --print( "Cheat Duplicate Exposed Plot Override 11", x, y, unitType, newUnit, overrideDestPlotId );
             x = overrideDestPlotX;
@@ -430,21 +394,26 @@ local function OnDuplicate(selectedPlayerId, unitId, unitType, overridePlayer, o
                 --y = plot:GetY();
             --end
         end
+
         if overridePlayer ~= nil then
             --print( "Cheat Duplicate Exposed Player Override 22 ", x, y, unitType, selectedPlayerId );
             selectedPlayerId = overridePlayer;
-        end		
+        end
+
         if overrideType ~= nil then
             --print( "Cheat Duplicate Exposed Type Override 33 ", x, y, unitType, selectedPlayerId );
             unitType = overrideType;
         end
+
         -- local typeName = UnitManager.GetTypeName(unit);
         --print( "Cheat Duplicate Exposed Executed ", x, y, unitType );
         --print( "Cheat Duplicate Exposed Executed 44", x, y, unitType, selectedPlayerId, ttype );
         local dupRepeat = 1;
+
         if count ~= nil and count > 1 then
             dupRepeat = count;
         end
+
         for i = dupRepeat, 1, -1 do
             if useValidLoc then 
                 newUnit = UnitManager.InitUnitValidAdjacentHex(selectedPlayerId, unitType, x, y, 40);
@@ -452,8 +421,10 @@ local function OnDuplicate(selectedPlayerId, unitId, unitType, overridePlayer, o
                 newUnit = UnitManager.InitUnit(selectedPlayerId, unitType, x, y);
             end
         end
+
         print( "Cheat Duplicate Exposed Executed", x, y, unitType, newUnit, selectedPlayerId, useValidLoc, count );
         --newUnit:GetExperience():ChangeExperience(unit:GetExperience():GetExperiencePoints());
+
         if unit:GetReligion() ~= nil and newUnit:GetReligion() ~= nil and unit:GetReligion():GetReligionType() ~= -1 then
             print( "Cheat Duplicate Exposed Executed Set Religion from unit", x, y, unitType, unit:GetReligion():GetReligionType(), newUnit:GetReligion():GetReligionType() );
             newUnit:GetReligion():SetReligionType(unit:GetReligion():GetReligionType());
@@ -468,6 +439,7 @@ local function OnDuplicate(selectedPlayerId, unitId, unitType, overridePlayer, o
         --tprint( unitt , 8 );
         --for k,v in pairs(unitt) do print (k,v) end
         RefreshGUI();
+
     end
 end
 
@@ -475,14 +447,15 @@ end
 local function OnCheatMovement(selectedPlayerId, unitId, disableRefreshRequest)
     -- local units = Players[selectedPlayerId]:GetUnits();
     -- local unit = units:FindID(unitId);
-    
     local unit = nil;
+
     if UnitManager then 
         unit = UnitManager.GetUnit( selectedPlayerId, unitId ); 
     end
     
     if unit ~= nil and Players[selectedPlayerId]:IsHuman() then
         local unitMovesRem = ExposedMembers.MOD_Cheat_Menu_Units.GetUnitMovesFromUI(selectedPlayerId, unitId);
+
         if (unitMovesRem+10) < targetMovePoints then
             print( "Cheat Movement Exposed Executed" );
             UnitManager.ChangeMovesRemaining(unit, 10);
@@ -491,6 +464,7 @@ local function OnCheatMovement(selectedPlayerId, unitId, disableRefreshRequest)
                 prevCheatMoveUnit = unitId;
                 prevCheatMovePoints = unitMovesRem+10;
             end
+
             if autorestoreMove and (unitMovesRem+10) < targetMovePoints then
                 UnitManager.RestoreMovement(unit);
                 UnitManager.ChangeMovesRemaining(unit, targetMovePoints);
@@ -502,6 +476,7 @@ local function OnCheatMovement(selectedPlayerId, unitId, disableRefreshRequest)
         elseif not autorestoreMove and prevCheatMoveUnit ~= nil and prevCheatMovePoints ~= nil then
             unit = UnitManager.GetUnit( selectedPlayerId, prevCheatMoveUnit ); 
             unitMovesRem = ExposedMembers.MOD_Cheat_Menu_Units.GetUnitMovesFromUI(selectedPlayerId, prevCheatMoveUnit);
+
             if unit and unitMovesRem > prevCheatMovePoints then
                 -- decrease move points to initial value after it was toggled off
                 UnitManager.ChangeMovesRemaining(unit, (prevCheatMovePoints-unitMovesRem) );
@@ -509,24 +484,29 @@ local function OnCheatMovement(selectedPlayerId, unitId, disableRefreshRequest)
                     --ExposedMembers.MOD_Cheat_Menu_Units.UiRefreshUnitPanel();
                 --end
             end
+
             prevCheatMoveUnit = nil;
             prevCheatMovePoints = nil;
         end
     end
+
     RefreshGUI(selectedPlayerId, unitId);
+
 end
 
 --******************************************************************************
 local function OnCheatPromote(selectedPlayerId, unitId)
     -- local units = Players[selectedPlayerId]:GetUnits();
     -- local unit = units:FindID(unitId);
-    
     local unit = nil;
+
     if UnitManager then 
         unit = UnitManager.GetUnit( selectedPlayerId, unitId ); 
     end
+
     if unit ~= nil and Players[selectedPlayerId]:IsHuman() and autorestorePromo then
         local reqExp = unit:GetExperience():GetExperienceForNextLevel();
+
         if reqExp > 0 then
             print( "Cheat Promotion Exposed Executed" );
             unit:GetExperience():ChangeExperience(reqExp);
@@ -541,6 +521,7 @@ local function OnCheatPromote(selectedPlayerId, unitId)
         end
         --ExposedMembers.MOD_Cheat_Menu_Units.UiRefreshUnitPanel();
         RefreshGUI();
+
     end
 end
 
@@ -548,23 +529,28 @@ end
 local function OnCheatAttack(selectedPlayerId, unitId, isSilent)
     -- local units = Players[selectedPlayerId]:GetUnits();
     -- local unit = units:FindID(unitId);
-    
     local unit = nil;
+
     if UnitManager then 
         unit = UnitManager.GetUnit( selectedPlayerId, unitId ); 
     end
+
     if unit ~= nil and Players[selectedPlayerId]:IsHuman() and autorestoreAttack then
         if isSilent == nil or not isSilent then
             print( "Cheat Attack Exposed Executed" );
         end
+
         UnitManager.RestoreUnitAttacks(unit);
         local unitMovesRem = ExposedMembers.MOD_Cheat_Menu_Units.GetUnitMovesFromUI(selectedPlayerId, unitId);
         UnitManager.ChangeMovesRemaining(unit, 1);
+
         if unitMovesRem < 8 then
             UnitManager.ChangeMovesRemaining(unit, 4);
         end
+
         --ExposedMembers.MOD_Cheat_Menu_Units.UiRefreshUnitPanel();
         RefreshGUI();
+
     end
 end
 
@@ -583,6 +569,7 @@ function Initialize()
 
     -- Gedemon's method for sharing functions between different contexts using the ExposedMembers table.
     if ( not ExposedMembers.MOD_Cheat_Menu_Units) then ExposedMembers.MOD_Cheat_Menu_Units = {}; end
+
     ExposedMembers.MOD_Cheat_Menu_Units.OnCheatHeal = OnCheatHeal;
     ExposedMembers.MOD_Cheat_Menu_Units.OnDuplicate = OnDuplicate;
     ExposedMembers.MOD_Cheat_Menu_Units.OnCheatMovement = OnCheatMovement;
@@ -599,7 +586,6 @@ function Initialize()
     ExposedMembers.MOD_Cheat_Menu_Units.ChangeAutorestorePromo = ChangeAutorestorePromo;
     ExposedMembers.MOD_Cheat_Menu_Units.ApplyAutorestoreMovementCheat = ApplyAutorestoreMovementCheat;
     ExposedMembers.MOD_Cheat_Menu_Units_Initialized = true;
-    
 end
 
 Initialize();
